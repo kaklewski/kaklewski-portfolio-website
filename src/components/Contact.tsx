7e8f199cdd650faee466904ec554ba7f56a35ebd
Oskar <oskarkaklewski@gmail.com>
2025-02-04 16:20:29 +0100

    feat: Add Netlify compatibility to the contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -14,7 +14,11 @@ import { IconSend } from '@tabler/icons-react'
 
 export default function Contact() {
   return (
-    <Container maxW='7xl' id='contact' py={{ base: 20, md: 36 }}>
+    <Container
+      maxW='7xl'
+      id='contact'
+      py={{ base: 20, md: 36 }}
+      backgroundImage='url(background.png)'>
       <Heading size='4xl' mb={4}>
         Contact Me
       </Heading>
@@ -26,19 +30,31 @@ export default function Contact() {
 
         <Card.Root variant='outline'>
           <Card.Body>
-            <form>
+            <form
+              name='contact'
+              method='POST'
+              data-netlify='true'
+              data-netlify-honeypot='bot-field'>
               <Stack gap={6}>
+                <Field.Root display='none'>
+                  <Field.Label>Name</Field.Label>
+                  <Input type='text' name='bot-filed' title='bot-field' />
+                </Field.Root>
                 <Field.Root required>
                   <Field.Label>Name</Field.Label>
-                  <Input placeholder='Jon Snow' type='text' />
+                  <Input placeholder='Jon Snow' type='text' name='name' />
                 </Field.Root>
                 <Field.Root required>
                   <Field.Label>Email</Field.Label>
-                  <Input placeholder='me@example.com' type='email' />
+                  <Input
+                    placeholder='me@example.com'
+                    type='email'
+                    name='email'
+                  />
                 </Field.Root>
                 <Field.Root required>
                   <Field.Label>Message</Field.Label>
-                  <Textarea placeholder='Lorem ipsum' rows={5} />
+                  <Textarea placeholder='Lorem ipsum' rows={5} name='message' />
                 </Field.Root>
                 <Button
                   variant='surface'
