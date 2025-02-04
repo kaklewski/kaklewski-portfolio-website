74afb48fac34bc47a0bb2f7bf1ef5c9f6891f54b
Oskar <oskarkaklewski@gmail.com>
2025-02-04 22:04:50 +0100

    feat: Adjust font size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,13 +9,15 @@ export default function Footer() {
     <Box as='footer' py={8} width='100%' borderTopWidth='1px'>
       <Container maxW='7xl'>
         <Flex justifyContent='space-between' alignItems='center'>
-          <Text fontSize='sm'>© {year} Oskar Kąklewski</Text>
+          <Text fontSize='sm' className='accent-font'>
+            © {year} Oskar Kąklewski
+          </Text>
           <Box>
             <IconButton
               rounded='full'
               variant='outline'
               aria-label='Back to the top'
-              onClick={() => scrollToSection('main')}>
+              onClick={() => scrollToSection('body')}>
               <IconArrowUpDashed stroke={1.75} />
             </IconButton>
           </Box>
