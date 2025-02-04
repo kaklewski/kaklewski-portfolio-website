e6cc4534bfbed51de2986ffad5779af2fef5915b
Oskar <oskarkaklewski@gmail.com>
2025-02-04 16:19:03 +0100

    feat: Add separators between sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Theme } from '@chakra-ui/react'
+import { Box, Separator, Theme } from '@chakra-ui/react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Skills from './components/Skills'
@@ -9,12 +9,14 @@ export default function App() {
   return (
     <Theme appearance='dark'>
       <Navbar />
-      <main>
+      <Box as='main'>
         <Hero />
         <Skills />
+        <Separator />
         <Projects />
+        <Separator />
         <Contact />
-      </main>
+      </Box>
     </Theme>
   )
 }
