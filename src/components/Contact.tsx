fde6a762894548923bda260107d60e5380916656
Oskar <oskarkaklewski@gmail.com>
2025-02-05 21:22:11 +0100

    feat: Add contact details

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,13 +4,21 @@ import {
   Card,
   Container,
   Field,
+  Flex,
   Heading,
   Input,
+  Link,
   SimpleGrid,
   Stack,
+  Text,
   Textarea,
 } from '@chakra-ui/react'
-import { IconSend } from '@tabler/icons-react'
+import {
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconMapPin,
+  IconSend,
+} from '@tabler/icons-react'
 
 export default function Contact() {
   return (
@@ -20,14 +28,10 @@ export default function Contact() {
       py={{ base: 20, md: 36 }}
       backgroundImage='url(background.png)'>
       <Heading size='4xl' mb={4}>
-        Contact Me
+        Contact Me || Say Hello
       </Heading>
 
-      <SimpleGrid columns={{ base: 1, md: 2 }} gap={6}>
-        <Box>
-          <div>elo</div>
-        </Box>
-
+      <SimpleGrid columns={{ base: 1, md: 2 }} gap={{ base: 16, md: 6 }}>
         <Card.Root variant='outline'>
           <Card.Body>
             <form
@@ -37,24 +41,28 @@ export default function Contact() {
               data-netlify-honeypot='bot-field'>
               <Stack gap={6}>
                 <Field.Root display='none'>
-                  <Field.Label>Name</Field.Label>
+                  <Field.Label>Bot</Field.Label>
                   <Input type='text' name='bot-filed' title='bot-field' />
                 </Field.Root>
                 <Field.Root required>
                   <Field.Label>Name</Field.Label>
-                  <Input placeholder='Jon Snow' type='text' name='name' />
+                  <Input placeholder='Skyler White' type='text' name='name' />
                 </Field.Root>
                 <Field.Root required>
                   <Field.Label>Email</Field.Label>
                   <Input
-                    placeholder='me@example.com'
+                    placeholder='skyler@gmail.com'
                     type='email'
                     name='email'
                   />
                 </Field.Root>
                 <Field.Root required>
                   <Field.Label>Message</Field.Label>
-                  <Textarea placeholder='Lorem ipsum' rows={5} name='message' />
+                  <Textarea
+                    placeholder='My name is Skyler White, yo!'
+                    rows={5}
+                    name='message'
+                  />
                 </Field.Root>
                 <Button
                   variant='surface'
@@ -69,6 +77,39 @@ export default function Contact() {
             </form>
           </Card.Body>
         </Card.Root>
+
+        <Flex justifyContent='center' alignItems='center'>
+          <Flex direction='column' gap={12} color='teal.400'>
+            <Box>
+              <Flex gap={1} alignItems='center'>
+                <IconMapPin stroke={1} size={50} />
+                <Text className='accent-font' color='white' fontSize='20px'>
+                  Gdańsk, Poland
+                </Text>
+              </Flex>
+            </Box>
+            <Box>
+              <Flex gap={1} alignItems='center'>
+                <IconBrandLinkedin stroke={1} size={50} />
+                <Link href='https://www.linkedin.com/in/oskar-kaklewski/'>
+                  <Text className='accent-font' fontSize='20px'>
+                    /in/oskar-kaklewski
+                  </Text>
+                </Link>
+              </Flex>
+            </Box>
+            <Box>
+              <Flex gap={1} alignItems='center'>
+                <IconBrandGithub stroke={1} size={50} />
+                <Link href='https://github.com/kaklewski'>
+                  <Text className='accent-font' fontSize='20px'>
+                    /kaklewski
+                  </Text>
+                </Link>
+              </Flex>
+            </Box>
+          </Flex>
+        </Flex>
       </SimpleGrid>
     </Container>
   )
