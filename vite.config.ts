57c6a63449286727f001b6a834c199315c515960
Oskar <oskarkaklewski@gmail.com>
2025-03-09 17:17:24 +0100

    fix: Add export workaround for Tabler Icons slowdown issue

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,11 @@ export default defineConfig({
       include: '**/*.svg',
     }),
   ],
+  resolve: {
+    alias: {
+      // Workaround for Tabler Icons slowdown issue
+      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+  },
 })
