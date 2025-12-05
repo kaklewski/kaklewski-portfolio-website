0d55861a08ddbef06fc7e30880016e9943c77848
Oskar Kąklewski <oskarkaklewski@gmail.com>
2025-12-05 19:12:53 +0100

    chore: Update icon libraries to latest versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
@@ -17,12 +17,4 @@ export default defineConfig({
             include: '**/*.svg',
         }),
     ],
-    resolve: {
-        alias: {
-            // Workaround for Tabler Icons slowdown issue
-            // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
-            '@tabler/icons-react':
-                '@tabler/icons-react/dist/esm/icons/index.mjs',
-        },
-    },
 });
