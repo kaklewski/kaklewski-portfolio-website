c63b95a69a5b5ea158eb4f951c26e01648d473dd
Oskar Kąklewski <oskarkaklewski@gmail.com>
2025-12-08 21:51:36 +0100

    chore: Install and setup i18n-next

---
+++ src/utils/i18n.ts
@@ -0,0 +1,27 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+const resources = {
+    en: {
+        translation: {
+            testMessage: 'Welcome to React and react-i18next',
+        },
+    },
+    pl: {
+        translation: {
+            testMessage: 'Witaj w React and react-i18next',
+        },
+    },
+};
+
+i18n.use(initReactI18next).init({
+    resources,
+    lng: 'en',
+    fallbackLng: 'en',
+
+    interpolation: {
+        escapeValue: false,
+    },
+});
+
+export default i18n;
